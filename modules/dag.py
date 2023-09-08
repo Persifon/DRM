8a7e00a590f71db8e0ccc926899ed6229be0c1ef
Ghost-ate-your-brains <ghost-brake-your-home@protonmail.com>
2023-09-08 12:54:56 +0300

    Deleted pybuilder

--- modules/dag.py
+++ modules/dag.py
@@ -1,3 +1,6 @@
+"""
+This module implement Directed Acyclic Graph structure with method helps sending data
+"""
 import random
 import json
 from collections import OrderedDict
@@ -5,26 +8,26 @@ from ctypes import c_longdouble
 
 from modules.block import Block
 
-class DAGValidationError(Exception):
-    pass
-
 class DAG:
+    """Implement Directed Acyclic Graph structure"""
     def __init__(self) -> None:
-        self.graph = OrderedDict()
-        b = Block()
-        b.data = "Genesis Block"
-        b.calculate_hash()
-        self.graph["Genesis Block"] = b
-    
-    def reset_graph(self) -> OrderedDict:
-        self.graph = OrderedDict()
-        return self.graph
-    
-    def send(self, sender: str, receiver: str, quantity: c_longdouble, block: Block) -> str:
+        self.graph = self.reset_graph()
 
+    @staticmethod
+    def reset_graph() -> OrderedDict:
+        """This method recreate graph with geensis block"""
+        graph = OrderedDict()
+        block = Block()
+        block.data = "Genesis Block"
+        block.calculate_hash()
+        graph["Genesis Block"] = block
+        return graph
+
+    def send(self, sender: str, receiver: str, quantity: c_longdouble, block: Block) -> str:
+        """This function 'send' some DRM currency to receiver"""
         if block.hash in self.graph.keys():
-            raise KeyError('node %s already exists' % block.hash)
-        
+            raise KeyError(f"node {block.hash} already exists")
+
         block.data = f"{sender}:{receiver}:{quantity}"
         leaves = self.all_leaves()
 
@@ -34,12 +37,12 @@ class DAG:
 
         self.graph[block.hash] = block
         return block.hash
-    
-    def push_raw_data(self, data: str, block: Block) -> str:
 
+    def push_raw_data(self, data: str, block: Block) -> str:
+        """This function push raw string data to BlockDAG"""
         if block.hash in self.graph.keys():
-            raise KeyError('node %s already exists' % block.hash)
-        
+            raise KeyError(f"node {block.hash} already exists")
+
         block.data = data
         leaves = self.all_leaves()
 
@@ -49,7 +52,6 @@ class DAG:
 
         self.graph[block.hash] = block
         return block.hash
-        
 
     def all_leaves(self) -> list[str]:
         """ Return a list of all leaves (nodes with no downstreams) """
@@ -57,17 +59,21 @@ class DAG:
 
     #TODO Надо сделать jsonization и dejsonization
     def to_json(self) -> str:
+        """Export DAG structure to json"""
         return json.dump(self.graph)
-    
+
     def from_json(self, json_dag: str) -> bool:
-        self.reset_graph()
+        """Import DAG structure to json"""
+        self.graph = OrderedDict()
         self.graph = json.loads(json_dag)
         return True
-    
-    def block(self, hash: str) -> Block:
-        return self.graph[hash]
+
+    def block(self, block_hash: str) -> Block:
+        """This method helps you get Block object, when you only know a hash"""
+        return self.graph[block_hash]
 
     def size(self) -> int:
+        """This method count a lenght of the graph object"""
         return len(self.graph)
 
     def __choose_leaves(self, leaves: list[str]) -> list:
